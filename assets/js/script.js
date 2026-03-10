e426c1ff24fb60fe77a85fa93cd2d5d5bf9c3814
jahangiralom86ffff <jahangiralom144155160@gmail.com>
2026-03-10 14:38:51 +0600

    all file add korlam

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,32 @@
-$('.parallax-window').parallax({imageSrc: 'assets/images/Error/breadcrumbs.png'});
+
+
+$(document).ready(function () {
+    // WOW
+	if (typeof WOW !== "undefined") {
+		new WOW().init();
+	}
+  	//wow js init end//
+
+	//counter all use//
+	$(document).ready(function(){
+		$('.count').counterUp({
+		delay:10,
+		time:1000
+		});
+	});
+	//counter end//
+	
+	//img parallax banner//
+	$('.parallax-window').parallax({imageSrc: 'assets/images/Error/breadcrumbs.png'});
+	//img paralllax end//
+
+	//img parallax banner//
+	$('.parallax-to').parallax({imageSrc: 'assets/images/project/project-bottom.png'});
+	//img paralllax end//
+
+});
 //modal and wow js file//
 $(document).ready(function () {
-
-  // WOW
-  if (typeof WOW !== "undefined") {
-    new WOW().init();
-  }
 
   // Bootstrap Modal
   const myModal = document.getElementById('myModal');
